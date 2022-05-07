8e32301260cda89ea75bc25874edcb98088b7e90
yashgupta9810 <yashg0621@gmail.com>
2022-05-07 09:20:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// import logo from './logo.svg';
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <div className="container">
+          <label>Name</label>
+          <input type="text" />
+          <label>Age</label>
+          <input type="number" />
+          <label>Position</label>
+          <input type="text" />
+          <label>Country</label>
+          <input type="text" />
+          <label>Wage(Year)</label>
+          <input type="text" />
+          <button>Add-employe</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
